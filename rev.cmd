88410545bfe5d23d6146937f901c67d417403c18
Robert Crossfield <robcrossfield@gmail.com>
2012-12-08 12:11:07 +1100

    not in svn now

--- rev.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-SubWCRev.exe . rev.1 rev.h
-SubWCRev.exe . installer\revInst.1 installer\revInst.nsh
--- rev.sh
+++
@@ -1,8 +0,0 @@
-echo -n 'const char *SVNREV = "' >../rev.h
-svn info | grep 'Last Changed Rev:' | awk -F': ' ' {print $2}' | tr -d '\n' >>../rev.h
-echo '";' >>../rev.h
-echo -n 'const char *SVNDATE = "' >>../rev.h
-svn info | grep 'Last Changed Date:' | awk -F': ' ' {print $2}' | tr -d '\n' >>../rev.h
-echo '";' >>../rev.h
-echo -n >>../rev.h
-
