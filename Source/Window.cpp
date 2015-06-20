fbb5a307271e423b5f267cc5b41819382c17a6b2
Robert Crossfield <robcrossfield@gmail.com>
2015-06-21 09:00:44 +1000

    Add intro data, and playback (missing drawn text) Add copyright,

--- Source/Window.cpp
+++ Source/Window.cpp
@@ -1,3 +1,25 @@
+/*
+ *  Cannon Fodder
+ *  ------------------------
+ *
+ *  Copyright (C) 2008-2015 Robert Crossfield <robcrossfield@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
 #include "stdafx.hpp"
 
 cWindow::cWindow() {
@@ -21,13 +43,8 @@ cWindow::~cWindow() {
 	SDL_Quit();
 }
 
-bool cWindow::InitWindow( const std::string& pWindowTitle, bool pExtended ) {
+bool cWindow::InitWindow( const std::string& pWindowTitle ) {
 	
-	if (pExtended) {
-		mDimensionWindow.mWidth += 200;
-		mDimensionWindow.mHeight += 200;
-	}
-
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		// TODO: Log error
 		//SDL_GetError();
