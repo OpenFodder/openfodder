7bc0fd33dde7b7b445474f10bde3bb99d9351bd8
Robert Crossfield <robcrossfield@gmail.com>
2015-08-09 09:56:20 +1000

    We now have sound effects

--- Source/Window.cpp
+++ Source/Window.cpp
@@ -63,7 +63,7 @@ bool cWindow::InitWindow( const std::string& pWindowTitle ) {
 	}
 
 	int audio_rate = 22050;
-	Uint16 audio_format = AUDIO_S16SYS;
+	Uint16 audio_format = AUDIO_U8;
 	int audio_channels = 2;
 	int audio_buffers = 4096;
  
