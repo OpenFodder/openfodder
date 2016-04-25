ba9cc7654c59a073fd22c7e1db1ebe5620f21759
Robert Crossfield <robcrossfield@gmail.com>
2016-04-25 15:53:38 +1000

    name more words

--- Source/UnknownData.hpp
+++ Source/UnknownData.hpp
@@ -1445,7 +1445,8 @@ const uint8 mCopyProtection_Word_Your2[] = {
 	0xFD, 0xB0, 0x1A, 0x44, 0xFF
 };
 
-const uint8 byte_449C6[] = {
+// EXTRANEOUS
+const uint8 mCopyProtection_Word_Extraneous[] = {
 	0xFD, 0xA5, 0x21, 0x60, 0x6F, 0x39, 0xA8, 
 	0xE5, 0xF1, 0xB0, 0xFF
 };
@@ -1470,11 +1471,13 @@ const uint8 mCopyProtection_Word_Your[] = {
 	0xFD, 0xB0, 0x1A, 0x44, 0xFF
 };
 
-const uint8 byte_449E6[] = {
+// FOLLOW
+const uint8 mCopyProtection_Word_Follow[] = {
 	0xFB, 0xB2, 0x37, 0x7D, 0x60, 0x3E, 0xFF
 };
 
-const uint8 byte_449ED[] = {
+// TYPES
+const uint8 mCopyProtection_Word_Types[] = {
 	0xF7, 0xA0, 0x07, 0x48, 0x56, 0xFF
 };
 
@@ -1509,13 +1512,13 @@ const uint8 mCopyProtection_Word_Pay[] = {
 const sCopyProtection mCopyProtection_Values[] = {
 	{ 4, 2, 1, 1, byte_449BD },
 	{ 4, 3, 2, 1, mCopyProtection_Word_Your2 },
-	{ 5, 1, 2, 1, byte_449C6 },
+	{ 5, 1, 2, 1, mCopyProtection_Word_Extraneous },
 	{ 5, 2, 1, 2, mCopyProtection_Word_Cannon },
 	{ 5, 3, 1, 1, mCopyProtection_Word_Send },
 	{ 6, 1, 1, 2, mCopyProtection_Word_You2 },
 	{ 6, 2, 3, 2, mCopyProtection_Word_Your },
-	{ 6, 3, 3, 2, byte_449E6 },
-	{ 9, 1, 1, 2, byte_449ED },
+	{ 6, 3, 3, 2, mCopyProtection_Word_Follow },
+	{ 9, 1, 1, 2, mCopyProtection_Word_Types },
 	{ 9, 1, 2, 1, byte_449F3 },
 	{ 10, 1, 1, 3, mCopyProtection_Word_Times },
 	{ 11, 1, 1, 4, mCopyProtection_Word_You },
