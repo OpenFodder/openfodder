87c06850c8474e6f7162e4fe621cd07e7a2963f7
Robert Crossfield <robcrossfield@gmail.com>
2017-03-12 17:38:52 +1100

    Update dates

--- Source/CopyProtection.hpp
+++ Source/CopyProtection.hpp
@@ -2,7 +2,7 @@
 *  Cannon Fodder
 *  ------------------------
 *
-*  Copyright (C) 2008-2015 Robert Crossfield <robcrossfield@gmail.com>
+*  Copyright (C) 2008-2017 Robert Crossfield
 *
 *  This program is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
