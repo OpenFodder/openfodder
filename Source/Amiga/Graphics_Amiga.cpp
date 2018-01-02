d5e479e56cbbc26d9820837584dc079036be6c66
Robert Crossfield <robcrossfield@gmail.com>
2018-01-03 07:48:04 +1100

    Update copyright notice for 2018

--- Source/Amiga/Graphics_Amiga.cpp
+++ Source/Amiga/Graphics_Amiga.cpp
@@ -2,7 +2,7 @@
 *  Open Fodder
 *  ---------------
 *
-*  Copyright (C) 2008-2017 Robert Crossfield
+*  Copyright (C) 2008-2018 Open Fodder
 *
 *  This program is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
