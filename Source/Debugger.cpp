8e33b39ae614e6f2e749b6eeef286c1d5546a101
Robert Crossfield <robcrossfield@gmail.com>
2019-04-07 18:48:55 +1000

    Dont overwrite the output stream if the console is already allocated

--- Source/Debugger.cpp
+++ Source/Debugger.cpp
@@ -39,12 +39,13 @@ void cDebugger::ConsoleOpen() {
 
 #ifdef WIN32
     static bool attached = AttachConsole(ATTACH_PARENT_PROCESS);
-    if (!attached)
-        attached = AllocConsole();
+	if (!attached) {
+		attached = AllocConsole();
 
-    FILE *stream, *stream2;
-    freopen_s(&stream, "CONIN$", "r", stdin);
-    freopen_s(&stream2, "CONOUT$", "w", stdout);
+		FILE *stream, *stream2;
+		freopen_s(&stream, "CONIN$", "r", stdin);
+		freopen_s(&stream2, "CONOUT$", "w", stdout);
+	}
 #endif
 }
 
