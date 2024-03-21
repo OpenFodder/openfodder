b47b56546e56b3b04762ccfcac5e5f011f7a8e1e
Robert Crossfield <robcrossfield@gmail.com>
2024-03-22 06:55:46 +1100

    Update notice date

--- Source/PC/Resource_PC_CD.cpp
+++ Source/PC/Resource_PC_CD.cpp
@@ -2,7 +2,7 @@
  *  Open Fodder
  *  ---------------
  *
- *  Copyright (C) 2008-2018 Open Fodder
+ *  Copyright (C) 2008-2024 Open Fodder
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
