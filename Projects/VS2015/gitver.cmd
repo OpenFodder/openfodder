4da0094cb72936764ba1db6cb3f7a64938154050
Robert Crossfield <robcrossfield@gmail.com>
2026-01-04 17:41:04 +1100

    remove old vs project

--- Projects/VS2015/gitver.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-if exist ..\..\.git\ (
-    git --version >nul 2>&1 && (
-	    git log -n 1 --pretty="const char* gitversion=\"%%h\";" > ..\..\Source\gitver.hpp
-    ) || (echo const char* gitversion="unknown install git";> ..\..\Source\gitver.hpp)
-) else (echo const char* gitversion="unknown no git folder";> ..\..\Source\gitver.hpp)
